ed1ed9c2e2569f66b1d6eb2cd3570628c87439ba
ciada <redcold96@gmail.com>
2018-12-09 02:59:50 +0200

    createCompany Done[TO MODULARIZE]

--- src/models/companies.js
+++ src/models/companies.js
@@ -1,20 +1,28 @@
 var mongoose = require('mongoose')
-var Schema = mongoose.Schema
 
-var companies = new Schema({
-    displayName: { type: String, required: true },
-    name: { type: String, unique: true, index: true },
-    workspaces: [workSpace]
+
+
+var user = new mongoose.Schema({
+    // email has to be unique withing the context of the workspace it belongs to
+    email: { type: String, index: true },
+    role: { type: String, enum: ['basic', 'admin'] }
 })
 
-var workSpace = new Schema({
+
+var workSpace = new mongoose.Schema({
     displayName: { type: String, required: true },
     //NAME HAS TO BE UNIQUE WITHIN THE COMPANY IT BELONGS TO -> Query?
     name: { type: String, index: true },
-    // email has to be unique withing the context of the workspace it belongs to
-    email: { type: String, index: true, unique: true },
-    role: { type: String, enum: ['basic', 'admin'] }
-})
+    users: [user]
+}, { timestamps: true })
+
+
+
+var companies = new mongoose.Schema({
+    displayName: { type: String, required: true },
+    name: { type: String, unique: true, index: true },
+    workspaces: [workSpace]
+}, { timestamps: true })
 
 
 // a pre-hook (s)to generate the the company name(and convert it to lowecase) on demand(before saving )
--- src/router.js
+++ src/router.js
@@ -1,23 +1,58 @@
 var Router = require('express').Router
 var router = new Router()
-    // eslint-disable-next-line indent
-    //var Book = require("./models/books_model")
-    //var controller = require('./controller')
 
-router.get('/', function(req, res) {
-    res.json({ msg: 'books' })
+var companies = require('./models/companies')
+var _ = require('lodash')
+
+router.post('/api/v1/createcompany', function(req, res) {
+    if (_.isEmpty(req.body)) {
+        return res.status(400).json({ success: false, message: 'Empty Request Body' })
+    }
+    var displayName = req.body.displayName
+    if (!displayName) {
+        return res.status(400).json({ success: false, message: 'Please enter a display name' })
+
+    }
+
+    companies.findOne({ name: displayName.toLowerCase() }, function(err, company) {
+        if (!err && company) {
+            return res.status(400).json({ success: false, message: 'A company with same name already exists' })
+        }
+
+        var newCompany = new companies({
+            displayName: displayName
+        })
+        newCompany.save(function(err, company) {
+            if (!err && company) {
+                return res.status(200).json({ success: true, message: 'Company created successfuly' })
+
+            }
+            return res.status(400).json({ success: false, message: err })
+
+        })
+
+    })
+
+
 })
 
-// router.get("/api/v1/books", async(req, res) => {
-//     var books = await Book.find({});
-//     res.json(books);
-// });
+// router.post('/api/v1/updatecompany', function(req, res) {
 
-// router.post("/api/v1/books", async(req, res) => {
-//     var book = new Book({ name: req.body.name });
-//     var savedBook = await book.save();
-//     res.json(savedBook);
-// });
+// })
+
+// router.post('/api/v1/company/creatworkspace', function(req, res) {
+
+// })
+// router.post('/api/v1/company/updateworkspace', function(req, res) {
+
+// })
+// router.post('/api/v1/company/workspace/addUser', function(req, res) {
+
+// })
+// router.post('/api/v1/company/workspace/deletUser', function(req, res) {
+
+// })
+
 
 
 
